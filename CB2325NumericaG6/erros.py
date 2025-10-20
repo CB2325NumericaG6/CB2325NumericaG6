430d0422999b851c0aae92fa35aad82a6acd0f0f
Mateus AO <al.mateus.oliveira@impatech.edu.br>
2025-10-20 11:27:47 -0300

    [feat]<Absolute error>: Implemented Absolute Error function

--- CB2325NumericaG6/erros.py
+++ CB2325NumericaG6/erros.py
@@ -1,8 +1,8 @@
 
 
 def erro_absoluto(valor_real: float, valor_aprox: float) -> float:
-    # TODO: Implement this
-    raise NotImplementedError
+    """Return the absolute Error of an aproximations by calculating the absolute of the difference between the values"""
+    return abs(valor_real-valor_aprox)
 
 def erro_relativo(valor_real: float, valor_aprox: float) -> float:
     # TODO: Implement this
