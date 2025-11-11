8d95d2c753d97ec18a75b78902092a6f92683404
ry4n-felinto <al.ryan.felinto@impatech.edu.br>
2025-11-10 23:00:37 -0300

    Implementação do método de integral por soma de Riemann

--- CB2325NumericaG6/integracao.py
+++ CB2325NumericaG6/integracao.py
@@ -1,7 +1,8 @@
 from typing import Callable
+from numpy import linspace
+# Falta implementar o linspace de core.py
 
-
-def integral(f:Callable, start: float, end: float, divisions: int) -> float:
+def integral_trapezio(f:Callable, start: float, end: float, divisions: int) -> float:
     """Esse método calcula a integral de uma função por aproximação trapezoidal
     Args:
         f (Callable): Função a ser integrada
@@ -30,3 +31,25 @@ def integral(f:Callable, start: float, end: float, divisions: int) -> float:
         i += Xincrement
     
     return sumVal
+
+def integral_riemann(f:Callable, start:float, end:float, divisions:int) -> float:
+    """Este método calcula a integral de uma função por
+    soma de Riemann
+    Args:
+        f (Callable): Função a ser integrada
+        start (float): Ponto inicial do intervalo
+        end (float): Ponto final do intervalo
+        divisions (int): Número de subdivisões do intervalo: números maiores implicam uma aproximação mais precisa, mas também consome mais CPU.
+    Returns:
+        float: Valor da integral.
+    Examples:
+        >>> f = lambda x: x**2
+        >>> i = integracao.integral_riemann(f, 0, 3, 1000)
+        >>> print(round(i,2))
+        9.0
+    """
+    base = abs(end - start)/divisions
+    retangulos = [base * f(x) for x in linspace(start+base/2,end-base/2, divisions)]
+    i = sum(retangulos)
+
+    return i
